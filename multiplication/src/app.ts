751acf4fb98b2bab1f403d267c2b7086284c9c31
Javier Gonzalez <javgon2306@gmail.com>
2024-07-17 00:05:29 -0300

    coment

--- multiplication/src/app.ts
+++ multiplication/src/app.ts
@@ -1,3 +1,16 @@
-const message: string = 'Hello World!';
 
-console.log(message);
+
+// intento para pintar en pantalla
+import * as fs from 'fs';
+import * as path from 'path';
+
+const filePath = path.join(__dirname, '../outputs/tabla-1.txt');
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err){
+    console.error('Error al leer el archivo:', err);
+    return;
+  }
+
+    console.log('Contenido del archivo:', data);
+});
